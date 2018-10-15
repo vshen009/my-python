aa0094c946d115387720341e935d36320d1b9aa8
Vincent S <3667050@qq.com>
2018-10-15 15:23:11 +0800

    add 123.py

---
+++ 123.py
@@ -0,0 +1,6 @@
+name = input('请输入名字:')
+high = input('please input high:(cm)')
+zhong = input('please input zhong:')
+print("Hello!",name ," your high is:",high,"cm. your zhoang liang is",zhong ,"KG")
+
+
