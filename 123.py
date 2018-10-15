259bb480f0b50ee27556f7645d0bf043676615e3
Vincent S <3667050@qq.com>
2018-10-15 20:47:32 +0800

    change 123.py again

--- 123.py
+++ 123.py
@@ -1,6 +1,6 @@
 name = input('请输入名字:')
 high = input('请输入高度:(cm)')
 zhong = input('请输入体重（KG）:')
-print("Hello!",name ," your high is:",high,"cm. your zhoang liang is",zhong ,"KG")
+print("Hello!",name ," your high is:",high,"cm. 你的体重是 ",zhong ,"KG")
 
 
