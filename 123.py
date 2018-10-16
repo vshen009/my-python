d80d9f1244b37a3c671b53b3e113c7a9c764e0a4
Vincent S <3667050@qq.com>
2018-10-16 14:57:59 +0800

    update coding style

--- 123.py
+++ 123.py
@@ -1,10 +1,12 @@
 #好的写法
 #coding style 写程序的风格,易读性很重要
+#, 配空格, 变量用英语单词,贴切,直觉, 全部小写
+# 连接两个单词用_ , my_name ,your_id ,his_pan_color
 
-
+X = 5
 name = input('请输入名字:')
-high = input('请输入高度:(cm)')
-zhong = input('请输入体重（KG）:')
-print("Hello!",name ," your high is:",high,"cm. 你的体重是 ",zhong ,"KG")
+height = input('请输入高度:(cm)')
+weight = input('请输入体重（KG）:')
+print("Hello!", name ," your high is:", height,"cm. 你的体重是 ", weight ,"KG")
 
 
