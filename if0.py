5d1898ac4d2f6b6e478fe693b92eff98cfeefdaf
Vincent S <3667050@qq.com>
2018-10-16 18:07:25 +0800

    add new py

--- if0.py
+++ if0.py
@@ -1,5 +1,8 @@
 rain = input( "请问今天有没有下雨: " )
+x = input( 'x 等于几:' )
+x = int(x)
 if rain == 'yes' :  #别忘记写: 
-	print ('撑伞出门')
-	print ('买一包薯片')
-	print ('在家看电影') 
+	print ('既然下雨就在家看电影吧')
+
+	if x > 3: #第二个if 需要空一行
+		print( 'x值大于三呢' )
---
+++ if1.py
@@ -0,0 +1,5 @@
+age = input('请输入年纪:')
+age = int(age)
+
+if age >= 10:
+	print( '你可以投票' )
